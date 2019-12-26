d79584af8814471c4f3aeb2c7f0867616565d85c
daniilprohorov <daniilprohorov@gmail.com>
2019-12-27 02:35:50 +0300

    fix minus

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ var count = parseInt(localStorage.getItem('count')) || 0;
 simply.title(count)
 simply.on('singleClick', 'up', function(e) {
   // simply.subtitle('You pressed the ' + e.button + ' button!');
+ 
   count += 1
   simply.title(count)
   localStorage.setItem('count', count);
 });
 simply.on('singleClick', 'down', function(e) {
   // simply.subtitle('You pressed the ' + e.button + ' button!');
-  count -= 1
+  if count > 0 {
+    count -= 1
+  }
   simply.title(count)
   localStorage.setItem('count', count);
 });
