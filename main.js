b1d49777ab44adb8026e35e0a4986b73b80348c1
daniilprohorov <daniilprohorov@gmail.com>
2019-12-27 02:58:05 +0300

    add scrollable

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 simply.fullscreen(true);
 simply.style('small');
+simply.scrollable(true);
 var count = parseInt(localStorage.getItem('count')) || 0; 
-var kek = [{title : 'Титул 1', text : 'лол кек чебурек'}, {title : 'Титул 2', text : 'лол кек чебурек 2'}];
+var data = [{title : 'Титул 1', text : 'лол кек чебурек'}, {title : 'Титул 2', text : 'лол кек чебурек 2'}];
 simply.title(count);
-simply.on('singleClick', 'up', function(e) {
+simply.on('singleClick', 'select', function(e) {
   // simply.subtitle('You pressed the ' + e.button + ' button!');
  
   count += 1;
-  simply.title(kek[0].title);
-  simply.body(kek[0].text);
+  simply.title(data[0].title);
+  simply.body(data[0].text);
   localStorage.setItem('count', count);
 });
-simply.on('singleClick', 'down', function(e) {
+simply.on('longClick', 'select', function(e) {
   // simply.subtitle('You pressed the ' + e.button + ' button!');
   if(count > 0) {
     count -= 1;
-    simply.title(kek[1].title);
-    simply.body(kek[1].text);
+    simply.title(data[1].title);
+    simply.body(data[1].text);
     localStorage.setItem('count', count);
   }
 });
