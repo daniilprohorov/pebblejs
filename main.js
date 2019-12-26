5badc702d699c61d5164d3ce38f0012f6772ecf4
daniilprohorov <daniilprohorov@gmail.com>
2019-12-27 02:31:48 +0300

    counter

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 simply.fullscreen(true)
+var count = 0
+simply.title(count)
 simply.on('singleClick', 'up', function(e) {
-  simply.subtitle('You pressed the ' + e.button + ' button!');
+  // simply.subtitle('You pressed the ' + e.button + ' button!');
+  count += 1
+});
+simply.on('singleClick', 'down', function(e) {
+  // simply.subtitle('You pressed the ' + e.button + ' button!');
+  count -= 1
 });
