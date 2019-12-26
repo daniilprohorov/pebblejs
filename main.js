27a61aef3cea66c77d673b48958a87614d1b8c59
daniilprohorov <daniilprohorov@gmail.com>
2019-12-27 02:34:40 +0300

    counter fix localStorage

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 simply.fullscreen(true)
-var count = 0
+var count = parseInt(localStorage.getItem('count')) || 0; 
 simply.title(count)
 simply.on('singleClick', 'up', function(e) {
   // simply.subtitle('You pressed the ' + e.button + ' button!');
   count += 1
   simply.title(count)
+  localStorage.setItem('count', count);
 });
 simply.on('singleClick', 'down', function(e) {
   // simply.subtitle('You pressed the ' + e.button + ' button!');
   count -= 1
   simply.title(count)
+  localStorage.setItem('count', count);
 });
