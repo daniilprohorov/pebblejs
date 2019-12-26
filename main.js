6e370db54319b84c8f61c0c7e2d083f0d10164d2
daniilprohorov <daniilprohorov@gmail.com>
2019-12-27 02:55:14 +0300

    maybe text work

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ simply.on('singleClick', 'up', function(e) {
   // simply.subtitle('You pressed the ' + e.button + ' button!');
  
   count += 1;
-  simply.title(kek[1].title);
-  simply.text(kek[1].text);
+  simply.title(kek[0].title);
+  simply.body(kek[0].text);
   localStorage.setItem('count', count);
 });
 simply.on('singleClick', 'down', function(e) {
   // simply.subtitle('You pressed the ' + e.button + ' button!');
   if(count > 0) {
     count -= 1;
-    simply.title(kek[2].title);
-    simply.text(kek[2].text);
+    simply.title(kek[1].title);
+    simply.body(kek[1].text);
     localStorage.setItem('count', count);
   }
 });
