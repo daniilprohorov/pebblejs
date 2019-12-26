f0793ab0f9fd613872b94d794bcb2a910c2aa153
daniilprohorov <daniilprohorov@gmail.com>
2019-12-27 02:41:49 +0300

    test var

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-simply.fullscreen(true)
+simply.fullscreen(true);
+simply.style('small');
 var count = parseInt(localStorage.getItem('count')) || 0; 
-simply.title(count)
+var kek = 2;
+simply.title(count);
 simply.on('singleClick', 'up', function(e) {
   // simply.subtitle('You pressed the ' + e.button + ' button!');
  
-  count += 1
-  simply.title(count)
+  count += 1;
+  simply.title(count);
   localStorage.setItem('count', count);
 });
 simply.on('singleClick', 'down', function(e) {
   // simply.subtitle('You pressed the ' + e.button + ' button!');
   if(count > 0) {
-    count -= 1
-    simply.title(count)
+    count -= 1;
+    simply.title(kek);
     localStorage.setItem('count', count);
   }
 });
