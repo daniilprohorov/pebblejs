9d02c1dcff6a7aaa7dded5df0a766611e17a78f6
daniilprohorov <daniilprohorov@gmail.com>
2019-12-27 02:43:41 +0300

    test array

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 simply.fullscreen(true);
 simply.style('small');
 var count = parseInt(localStorage.getItem('count')) || 0; 
-var kek = 2;
+var kek = ['lol', 'kek', 'petuh', 'lulz'];
 simply.title(count);
 simply.on('singleClick', 'up', function(e) {
   // simply.subtitle('You pressed the ' + e.button + ' button!');
  
   count += 1;
-  simply.title(count);
+  simply.title(kek[count]);
   localStorage.setItem('count', count);
 });
 simply.on('singleClick', 'down', function(e) {
   // simply.subtitle('You pressed the ' + e.button + ' button!');
   if(count > 0) {
     count -= 1;
-    simply.title(kek);
+    simply.title(kek[count]);
     localStorage.setItem('count', count);
   }
 });
